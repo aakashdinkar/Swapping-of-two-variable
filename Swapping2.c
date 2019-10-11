f7631c0508eac0e062e978ca0a0e6a8066bc33ce
Aakash Dinkar <35952953+aakashdinkar@users.noreply.github.com>
2019-10-11 12:08:57 +0530

    Swapping2

---
+++ Swapping2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(l
+{
+ int a,b;
+ printf("Enter two numbers a and b");
+ scanf("%d%d",a,b);
+ a=a^b;
+ b=a^b;
+ a=a^b;
+ printf(%numbers after swapping\n");
+ printf("%d %d",a,b);
+ return 0;
+}
