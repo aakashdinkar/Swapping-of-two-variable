449246ca6d56783a8ed8d662e2fc81dfc82905f9
Aakash Dinkar <35952953+aakashdinkar@users.noreply.github.com>
2019-10-16 19:42:10 +0530

    Swapping.cpp

---
+++ swapping.cpp
@@ -0,0 +1,13 @@
+#include<iostream.h>
+int main()
+{
+ int a,b;
+ cout<<"Enter two numbers a and b";
+ cin>>a>>b;
+ a=a+b;
+ b=a-b;
+ a=a-b;
+ cout<<"Numbers after swapping\n";
+ cin>>a>>b;
+ return 0;
+}
